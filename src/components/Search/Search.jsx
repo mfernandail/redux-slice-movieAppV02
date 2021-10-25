efd0452de5722623337eeec256cf18b039452c43
MariaFernanda <maria.ibanez.leiva@gmail.com>
2021-10-25 20:54:15 -0300

    Intento 5.2

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 import { fetchSeriesAsync, fetchMoviesAsync, getAllMovies } from '../../redux/movies/movieSlice';
 import { ShowListening } from '../../components/ShowListening/ShowListening';
 import { InputSearch } from '../inputSearch/InputSearch';
@@ -8,10 +8,15 @@ import { Spinner } from '../Spinner/Spinner';
 import './Search.css';
 
 export const Search = () => {
-  console.log('details')
+
   const {searchShow} = useParams();
   const dispatch = useDispatch();
   const data = useSelector(getAllMovies);
+
+  console.log('params ', searchShow)
+
+  const location = useLocation();
+  console.log('location ', location)
 
   const searchLocal = localStorage.getItem('search') || '';
   const busqueda = searchShow  || searchLocal
