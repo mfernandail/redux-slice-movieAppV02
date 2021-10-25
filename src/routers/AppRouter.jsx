730ecf1a8791ba24283ac57d92dcea64549565fc
MariaFernanda <maria.ibanez.leiva@gmail.com>
2021-10-25 17:07:35 -0300

    Intento 3

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -6,46 +6,30 @@ import {
   Redirect
 } from 'react-router-dom';
 import { Header } from '../components/Header/Header';
-import { Home } from '../pages/Home/Home';
-import { Detail } from '../pages/Detail/Detail';
 import { Footer } from '../components/Footer/Footer';
-import { PageNotFound } from '../pages/Error/Error404';
-import { Search } from '../components/Search/Search';
+// import { Home } from '../pages/Home/Home';
+// import { Detail } from '../pages/Detail/Detail';
+// import { PageNotFound } from '../pages/Error/Error404';
+// import { Search } from '../components/Search/Search';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
 import { fetchMoviesAsync, fetchSeriesAsync } from '../redux/movies/movieSlice';
+import { RoutesList } from './RoutesList';
 
 export const AppRouter = () => {
-  const dispatch = useDispatch();
-  const {searchShow = ''} = useParams();
+  // const dispatch = useDispatch();
+  // const {searchShow = ''} = useParams();
 
-
-  useEffect(() => {
-    dispatch(fetchMoviesAsync(searchShow))
-    dispatch(fetchSeriesAsync(searchShow))
-  }, [])
+  // useEffect(() => {
+  //   dispatch(fetchMoviesAsync())
+  //   dispatch(fetchSeriesAsync())
+  // }, [])
 
   return (
     <Router>
       <Header />
       <div className="container">      
-        <Switch>
-          <Route
-            path="/movie/:imdbid"
-            component={Detail}
-          />
-          <Route
-            path="/search/:searchShow"
-            component={Search}
-            exact
-          />          
-          <Route
-            path="/"
-            exact
-            component={Home}
-          />
-          <Redirect to="/" />
-        </Switch>
+        <RoutesList />
       </div>
       <Footer />
     </Router>
---
+++ src/routers/RoutesList.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { Redirect, Route, Switch, useParams } from 'react-router';
+import { Home } from '../pages/Home/Home';
+import { Detail } from '../pages/Detail/Detail';
+import { Search } from '../components/Search/Search';
+import { useDispatch } from 'react-redux';
+import { fetchMoviesAsync, fetchSeriesAsync } from '../redux/movies/movieSlice';
+
+
+export const RoutesList = () => {
+  const dispatch = useDispatch();
+  const {searchShow = ''} = useParams();
+  
+  useEffect(() => {
+    dispatch(fetchMoviesAsync(searchShow))
+    dispatch(fetchSeriesAsync(searchShow))
+  }, [])
+
+
+  return (
+    <Switch>
+      <Route
+        path="/movie/:imdbid"
+        component={Detail}
+      />
+      <Route
+        path="/search/:searchShow"
+        component={Search}
+        exact
+      />          
+      <Route
+        path="/"
+        exact
+        component={Home}
+      />
+      <Redirect to="/" />
+    </Switch>
+  )
+}
