ddeffadda706b00a86e8176e8312fec17536d7ac
MariaFernanda <maria.ibanez.leiva@gmail.com>
2021-10-26 20:51:03 -0300

    03

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  //plugins: [reactRefresh()]
+  plugins: [pluginRewriteAll()]
 })
