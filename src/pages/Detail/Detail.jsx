2328e3b641fdd3c8b898790a8fdb8e42ad11a1a2
MariaFernanda <maria.ibanez.leiva@gmail.com>
2021-10-25 22:21:57 -0300

    Prueba 0

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
-import { fetchAsyncMovieOrShowDetail, fetchShowDetailsAsync, getAllSelected, removeSelectedShow } from '../../redux/movies/movieSlice';
+import { fetchAsyncMovieOrShowDetail, getAllSelected, removeSelectedShow } from '../../redux/movies/movieSlice';
 import { DetailsShow } from '../../components/DetailsShow/DetailsShow';
 import { Spinner } from '../../components/Spinner/Spinner';
 
